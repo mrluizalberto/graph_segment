015b21ef9885a5e0cfd3e56e6c995eb0bb3ec328
Luiz Alberto <C202@petrobras.com.br>
2017-02-22 10:35:41 -0300

    adicionando CMakeLists.txt

--- segment.cpp
+++ segment.cpp
@@ -14,6 +14,9 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+
+
+Adapted to handle 3D images by Luiz Alberto Lima - EXP/GEOP/TGEO - Jan/2017
 */
 
 #include <cstdio>
